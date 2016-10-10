bcd9b8f410b87a7db671542bac6ccf31cb0bd60c
Jesse Parker <jp00879@outlook.com>
2016-10-10 12:30:23 -0500

    Small tweaks clean up css

--- app/components/Login/Login.jsx
+++ app/components/Login/Login.jsx
@@ -5,46 +5,59 @@ import RaisedButton from 'material-ui/RaisedButton';
 import AppBar from 'material-ui/AppBar';
 
 const style = {
-  height: 250,
-  margin: 0,
-  textAlign: 'center',
-  display: 'flex'
+    alignItems: 'center',
+    display: 'flex',
+    position: 'absolute',
+    width: '300px',
+    height: '100px',
+    left: '50%',
+    top:'50%',
+    marginLeft:'-150px',
+    marginTop: '-50px' 
+}
+
+const textfieldStyle= {
+    marginLeft: '20',
+    marginRight: '20',
+    marginTop: '20'
+}
+
+const paperStyle = {
+    height: 300,
+    margin: 0,
+    textAlign: 'center',
+    display: 'flex'
 };
-const style2 = {
-    margin: 20,
-    marginLeft: 160,
-    marginTop: 50
+const buttonStyle = {
+    margin: 0,
+    marginTop: 40,
+    width: '50%'
 }
 export default class Login extends React.Component {
 
     render() {
         return (
-            <div style={{alignItems: 'center',display: 'flex',position: 'absolute', width: '300px', height: '100px',left: '50%',top:'50%', marginLeft:'-150px', marginTop: '-50px'}}>
-                <Paper style={style} zDepth={3} rounded={true}  >
-                 
-                    <div style={{width: '100%',marginBottom: '45px'}}> 
+            <div style={style}>
+                <Paper style={paperStyle} zDepth={3} rounded={true}  >
+                    <div> 
                         <AppBar
                             title="PM-POS"
                             showMenuIconButton={false}
                         />                    
                         <TextField
-                            style={{marginLeft: '20', marginRight: '20'}}
+                            style={textfieldStyle}
                             hintText="Pin Number"
                             floatingLabelText="Enter Your Pin"
                             type="password"
                         />
-                        <div style={{alignItems: 'center'}}>
                         <RaisedButton 
                             primary={true} 
                             label="LogIn" 
-                            style={style2} 
-                        /></div>
-                        
+                            style={buttonStyle} 
+                        />
                     </div>
-                
                 </Paper>
             </div>
-
         );
     }
 }
