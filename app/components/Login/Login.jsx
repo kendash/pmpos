e2ed39bd10c18bbdb942823c90e153c7f5699e38
Jesse Parker <jp00879@outlook.com>
2016-10-11 10:52:09 -0500

    Small tweaks to raised button

--- app/components/Login/Login.jsx
+++ app/components/Login/Login.jsx
@@ -2,65 +2,128 @@ import React from 'react';
 import Paper from 'material-ui/Paper';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
-import AppBar from 'material-ui/AppBar';
+import {grey900, grey400, grey200, cyan500, blue500} from 'material-ui/styles/colors';
+import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
+import MenuItem from 'material-ui/MenuItem';
+import IconMenu from 'material-ui/IconMenu';
+import IconButton from 'material-ui/IconButton';
+import NavigationExpandMoreIcon from 'material-ui/svg-icons/navigation/expand-more';
 
 const style = {
-    alignItems: 'center',
-    display: 'flex',
-    position: 'absolute',
-    width: '300px',
-    height: '100px',
-    left: '50%',
-    top:'50%',
-    marginLeft:'-150px',
-    marginTop: '-50px' 
-}
+    mainStyle: {
+        alignItems: 'center',
+        display: 'flex',
+        position: 'absolute',
+        width: '300px',
+        height: '100px',
+        left: '50%',
+        top:'50%',
+        marginLeft:'-150px',
+        marginTop: '-50px', 
+        backgroundColor: blue500
+    },
+    textFieldStyle: {
+        marginLeft: '20',
+        marginRight: '20',
+        marginTop: '20'
+    },
+    textFieldStyle2: {
+        marginLeft: '20',
+        marginRight: '20',
+        marginTop: '0'
+    },
+    paperStyle: {
+        height: 350,
+        margin: 0,
+        textAlign: 'center'
+    },
+    buttonStyle: {
+        margin: 0,
+        marginTop: 60,
+        marginLeft: 20,
+        marginRight: 20
+    },
+    hintStyle: {
+        color: blue500
+    },
+    floatingLabelFocusStyle: {
+        color: blue500
+    },
+    underlineFocusStyle: {
+        borderColor: blue500
+    },
+    loginTriangle: {
+        width: 0,
+        marginRight: 'auto',
+        marginLeft: 'auto',
+        border: '12px solid transparent',
+        borderBottomColor: blue500,
+        color: blue500,
+        marginTop: -25
 
-const textfieldStyle= {
-    marginLeft: '20',
-    marginRight: '20',
-    marginTop: '20'
-}
+    },
+    loginHeader: {
+        background: blue500,
+        padding: '20px',
+        fontSize: '1.4em',
+        fontWeight: 'normal',
+        textAlign: 'center',
+        color: '#fff',
+        margin: 0
 
-const paperStyle = {
-    height: 300,
-    margin: 0,
-    textAlign: 'center',
-    display: 'flex'
+    }
 };
-const buttonStyle = {
-    margin: 0,
-    marginTop: 40,
-    width: '50%'
-}
+
+
 export default class Login extends React.Component {
+
+    componentWillMount(){
+        document.body.style.backgroundColor = grey400;
+    }
+
+    componentWillUnmount(){
+        document.body.style.backgroundColor = null;
+    }
 
     render() {
         return (
-            <div style={style}>
-                <Paper style={paperStyle} zDepth={3} rounded={true}  >
-                    <div> 
-                        <AppBar
-                            title="PM-POS"
-                            showMenuIconButton={false}
-                        />                    
-                        <TextField
-                            style={textfieldStyle}
-                            hintText="Pin Number"
-                            floatingLabelText="Enter Your Pin"
-                            type="password"
-                        />
-                        <RaisedButton 
-                            primary={true} 
-                            label="LogIn" 
-                            style={buttonStyle} 
-                        />
-                    </div>
-                </Paper>
+            <div>
+                <div style={style.mainStyle}>
+                    <Paper style={style.paperStyle} zDepth={2} rounded={false}  >
+                        <div> 
+                            <div style={style.loginTriangle}></div>
+                            <div style={style.loginHeader}>Log in to PM-POS</div>
+                            <TextField
+                                style={style.textFieldStyle}
+                                hintText="Username"
+                                floatingLabelText="Username"
+                                floatingLabelFocusStyle={style.floatingLabelFocusStyle}
+                                underlineFocusStyle={style.underlineFocusStyle}
+                            />
+                            <TextField
+                                style={style.textFieldStyle2}
+                                hintText="Password"
+                                floatingLabelText="Password"
+                                type="password"
+                                floatingLabelFocusStyle={style.floatingLabelFocusStyle}
+                                underlineFocusStyle={style.underlineFocusStyle}
+                            />
+                            <div style={style.buttonStyle}>
+                            <RaisedButton 
+                                fullWidth={true}
+                                label="Submit" 
+                                backgroundColor={blue500}
+                                labelColor="#fff"
+                            />
+                            </div>
+                        </div>
+                    </Paper>
+                </div>
             </div>
         );
     }
 }
+
 Login.contextTypes = {
     router: React.PropTypes.object
 };
